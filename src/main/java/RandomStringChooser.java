3e4763b516ab1fc357875d46f3460a419f90a2fa
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-04-23 21:50:20 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -3,16 +3,19 @@ public class RandomStringChooser
 {
   /* to be implemented in part (a) */
   private ArrayList<String> words;
+
   public RandomStringChooser(String[] wordArray)
   {
     words = new ArrayList<String>();
-  for (String w : wordArray)
-    words.add(w);
+    for(String w : wordArray)
+      words.add(w);
   }
-  public String getNext(){
-  if (words.size() == 0)
-    return "NONE";
-    int i = (int)(Math.random()*words.size());
+
+  public String getNext()
+  {
+    if (words.size() == 0)
+      return "NONE";
+    int i = (int)(Math.random() * words.size());
     return words.remove(i);
   }
   //Heads up! 
